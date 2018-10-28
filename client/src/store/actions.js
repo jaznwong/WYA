adbdd112f28a27bbdb2de722edfaa5655bc11a12
shikdernyc <shikdernyc@gmail.com>
2018-10-27 22:54:56 -0400

    Refractored some codes and catching error

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -3,19 +3,19 @@ export const SEARCH_ROOMS = "SEARCH_ROOMS"
 // TODO: Make this into an env variable
 const apiRoute = "/"
 
-function searchRoomAction(searchedRooms){
+function searchRoomsAction(searchedRooms){
     return{
         type: SEARCH_ROOMS,
         searchedRooms
     }
 }
 
-export function searchRoom(query){
+export function searchRooms(query){
     return dispatch => {
         let url = apiRoute + "rooms/" + query;
-        fetch(url)
-        .then(res => res.json())
-        .then(rooms=> dispatch(searchRoomAction(rooms)))
+        return fetch(url)
+            .then(res => res.json())
+            .then(rooms => dispatch(searchRoomsAction(rooms)))
+            .catch(err => console.log("Error in fetching searchRooms"))
     }
 }
-
