0db89d2c6e1b5658ff682bb9a0d6d80180276754
shikdernyc <shikdernyc@gmail.com>
2018-10-27 22:55:42 -0400

    Updated imports

--- client/src/components/dashboard/SearchForm.js
+++ client/src/components/dashboard/SearchForm.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {searchRooms} from '../store/actions'
+import {searchRooms} from '../../store/actions'
 
 class SearchForm extends Component{
     handleSearch(event){
