3d6f03a6c7e8f3bfbb8bfa4bb0d7cbbe30045faf
Asif Shikder <shikdernyc@gmail.com>
2018-10-29 21:52:57 +0000

    Created a basic login and signup component

---
+++ client/src/components/AuthForm.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react'
+
+class AuthForm extends Component{
+    constructor(props){
+        super(props)
+    }
+    
+    handleSubmit(event){
+        let {signup} = this.props
+        event.preventDefault()
+        // TODO: Handle Registration and Signin
+    }
+    
+    render(){
+        let {header, buttonText} = this.props
+        return(
+            <div>
+                 {header}
+                  <form onSubmit={this.handleSubmit.bind(this)}>
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Email address</label>
+                      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                      <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleInputPassword1">Password</label>
+                      <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    </div>
+                    <div className="form-group form-check">
+                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                      <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+                    </div>
+                    <button type="submit" className="btn btn-primary">{buttonText}</button>
+                  </form>
+            </div>
+        )
+    }
+}
+
+export default AuthForm
