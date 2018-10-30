ba5986e7070f40e6a1f377c7465e0c4461dc41b0
Asif Shikder <shikdernyc@gmail.com>
2018-10-30 19:20:56 +0000

    Created a login form to handle authentication

--- client/src/components/AuthForm.js
+++ client/src/components/AuthForm.js
@@ -1,36 +1,52 @@
 import React, {Component} from 'react'
+import {authUser} from '../store/actions'
+import {connect} from 'react-redux'
 
 class AuthForm extends Component{
     constructor(props){
         super(props)
+        this.state = {
+            warningMessage: ""
+        }
     }
     
     handleSubmit(event){
         let {signup} = this.props
         event.preventDefault()
-        // TODO: Handle Registration and Signin
+        let username = document.getElementById('username').value
+        let password = document.getElementById('password').value
+        authUser(signup, username, password)
+            .then(()=>{
+                this.setState({warningMessage: "Error"})
+                this.props.history.push('/')
+            })
+            .catch(()=>{
+                this.setState({warningMessage: "Error"})
+            })
     }
     
     render(){
         let {header, buttonText} = this.props
         return(
             <div>
-                 {header}
+                 <h2>{header}</h2>
+                    {this.state.warningMessage.length > 0 ? 
+                        (<div class="alert alert-primary" role="alert">
+                            A simple primary alert—check it out!
+                        </div>)
+                        :
+                        (<div></div>)
+                    }
                   <form onSubmit={this.handleSubmit.bind(this)}>
                     <div className="form-group">
-                      <label htmlFor="exampleInputEmail1">Email address</label>
-                      <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                      <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
+                      <label htmlFor="username">Username</label>
+                      <input type="text" className="form-control" id="username" placeholder="Username" />
                     </div>
                     <div className="form-group">
-                      <label htmlFor="exampleInputPassword1">Password</label>
-                      <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                      <label htmlFor="password">Password</label>
+                      <input type="password" className="form-control" id="password" placeholder="Password" />
                     </div>
-                    <div className="form-group form-check">
-                      <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                      <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
-                    </div>
-                    <button type="submit" className="btn btn-primary">{buttonText}</button>
+                    <button type="submit" className="btn btn-secondary">{buttonText}</button>
                   </form>
             </div>
         )
