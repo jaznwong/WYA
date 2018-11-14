590d6695f19a67bcce2fdd3830af3b8c6e6dbcd2
Justin Pacquing <pacqqu@gmail.com>
2018-11-14 18:51:31 -0500

    added room models and routes to create, modify, delte, and join room

--- wya-api/controllers/room.js
+++ wya-api/controllers/room.js
@@ -1,65 +1,186 @@
-const express = require('express');
-const models = require('../models');
-const Sequelize = require('sequelize');
+let Router = require('express').Router()
+let {
+    findById,
+    getAll,
+    create,
+    deleteAll,
+    findByRoomname,
+    addUser,
+    getUsers,
+    isUserInRoom,
+    deleteById
+} = require('../handlers/room')
+let isAuthenticated = require('../middlewares.js')
 
-const router = express.Router();
-
-
-router.get('/hardcode', (req, res) => {
-  let hardCodedRooms = []
-  for (let i = 0; i < 5; i++){
-    hardCodedRooms.push(models.Rooms.create({
-      name: `Test Room ${i}`,
-      isFinalized: false
-    }));
-  }
-  Promise.all(hardCodedRooms).then(function(data){
-    res.json({
-      msg: "Successfully hardcoded Rooms"
-    });
-  });
-});
-
-router.get('/', (req, res) => {
-  models.Rooms.findAll().then(rooms => {
-    res.json({
-      roomList: rooms
+// For debugging
+Router.route('/')
+    .get(function (req, res, next) {
+        getAll()
+            .then(rooms => {
+                res.status(200).json(rooms)
+            })
+            .catch(error =>{
+                next({
+                    status: 400,
+                    message: "Unable to retrieve all rooms"
+                })
+            })
     })
-  });
-});
 
-router.get('/:searchQuery', (req, res) => {
-  models.Rooms.findAll({
-    where: {
-      name: {[Sequelize.Op.like] : `%${req.param('searchQuery')}%`}
-    }
-  }).then(rooms => {
-    res.json({
-      roomList: rooms
+// For debugging
+Router.route('/')
+    .delete(function(req, res, next){
+        deleteAll()
+            .then(()=>{
+                res.status(200).json("message: All rooms have been deleted")
+            })
+            .catch(err=>{
+                next({
+                    status: 400,
+                    message: "Unable to delete all rooms"
+                })
+            })
     })
-  });
-});
 
-router.post('/', (req, res) => {
-  res.json({
-    msg: "Successful POST to '/' route"
-  });
-});
+/*
+// Expected Post Body:
+// req.body.roomname - STRING of roomname *REQUIRED*
+// req.body.description - STRING of room description
+*/
+Router.route('/')
+    .post(function(req, res, next){
+        isAuthenticated(req, res, function(user, err){
+            if(user){
+                let createParams = {
+                    roomname: req.body.roomname,
+                    description: req.body.description,
+                    creatorID: user.id,
+                    roomstatus: 'OPEN'
+                }
+                create(createParams).then((room) => {
+                    addUser(room, user).then((data) => {
+                        res.json(data)
+                    })
+                })
+                .catch((err) => {res.status(401).json(err.message)})
+            }
+            else res.status(401).json(err.message)
+        })
+    })
 
-router.put('/:id', (req, res) => {
-  res.json({
-    msg: "Successful PUT to '/' route",
-    id: req.params.id
-  });
-});
+Router.route('/:roomID')
+    .get(function(req, res, next){
+        isAuthenticated(req, res, function(user, err){
+            if(user){
+                findById(req.params.roomID)
+                .then((room) => {
+                    isUserInRoom(room, user.id)
+                    .then((isInRoom) => {
+                        if (isInRoom) res.json(room)
+                        else res.status(403).json("Not Part of Room")
+                    })
+                    .catch((err) => {res.status(401).json(err.message)})
+                })
+                .catch((err) => {res.status(401).json(err.message)})
+            }
+            else res.status(401).json(err.message)
+        })
+    })
 
-router.delete('/', (req, res) => {
-  models.Rooms.destroy({where: {}}).then(function () {
-    res.json({
-      msg: "Deleted All Rooms",
-    });
-  });
-});
+/*
+// Expected Post Body:
+// req.body.roomname - STRING of roomname *REQUIRED*
+// req.body.description - STRING of room description
+*/
+Router.route('/:roomId')
+    .patch(function(req, res, next){
+        isAuthenticated(req, res, function(user, err){
+            if(user){
+                findById(req.params.roomId)
+                .then((room) => {
+                    if (room.creatorID == user.id){
+                        let updatedRoomname = room.roomname
+                        if (req.body.roomname) updatedRoomname = req.body.roomname
+                        let updatedDescription = room.description
+                        if (req.body.description) updatedDescription = req.body.description
+                        room.update({
+                            roomname: updatedRoomname,
+                            description: updatedDescription
+                        })
+                        .then((room) => {res.json(room)})
+                        .catch((err) => {res.status(401).json(err.message)})
+                    }
+                })
+                .catch((err) => {res.status(401).json(err.message)})
+            }
+            else res.status(401).json(err.message)
+        })
+    })
 
+    /*
+    // Expected Post Body:
+    // req.body.roomname - STRING of roomname *REQUIRED*
+    // req.body.description - STRING of room description
+    */
+    Router.route('/:roomId')
+        .patch(function(req, res, next){
+            isAuthenticated(req, res, function(user, err){
+                if(user){
+                    findById(req.params.roomId)
+                    .then((room) => {
+                        if (room.creatorID == user.id){
+                            let updatedRoomname = room.roomname
+                            if (req.body.roomname) updatedRoomname = req.body.roomname
+                            let updatedDescription = room.description
+                            if (req.body.description) updatedDescription = req.body.description
+                            room.update({
+                                roomname: updatedRoomname,
+                                description: updatedDescription
+                            })
+                            .then((room) => {res.json(room)})
+                            .catch((err) => {res.status(401).json(err.message)})
+                        }
+                        else res.status(403).json("Not Creator of Room")
+                    })
+                    .catch((err) => {res.status(401).json(err.message)})
+                }
+                else res.status(401).json(err.message)
+            })
+        })
 
-module.exports = router;
+Router.route('/:roomId')
+    .delete(function(req, res, next){
+        isAuthenticated(req, res, function(user, err){
+            if(user){
+                findById(req.params.roomId)
+                .then((room) => {
+                    if (room.creatorID == user.id){
+                        deleteById(room.id)
+                        .then(() => {res.json("Room deleted")})
+                        .catch(() => {res.json("Room unable to be deleted")})
+                    }
+                    else res.status(403).json("Not Creator of Room")
+                })
+                .catch((err) => {res.status(401).json(err.message)})
+            }
+            else res.status(401).json(err.message)
+        })
+    })
+
+Router.route('/join/:roomId')
+    .get(function(req, res, next){
+        isAuthenticated(req, res, function(user, err){
+            if(user){
+                findById(req.params.roomId)
+                .then((room) => {
+                    addUser(room, user)
+                    .then((data) => {res.json(data)})
+                    .catch((err) => {res.status(401).json(err.message)})
+                })
+                .catch((err) => {res.status(401).json(err.message)})
+            }
+            else res.status(401).json(err.message)
+        })
+    })
+
+module.exports = Router
