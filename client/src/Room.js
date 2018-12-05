e46ad345a2cbbe9af1684bce0b03e105b69c127c
Asif Shikder <32821894+shikdernyc@users.noreply.github.com>
2018-12-04 19:20:09 -0500

    Update Room.js

--- client/src/Room.js
+++ client/src/Room.js
@@ -1,114 +1,123 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import {handleSuggestInterests, postInterests, postAvailablity} from './store/actions/user'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  handleSuggestInterests,
+  postInterests,
+  postAvailablity
+} from "./store/actions/user";
+import {Card, CardBody, CardImg, CardText, CardTitle, Button} from 'reactstrap'
 
-const UserCard = function({name}){
-    return (
-        <div className="card text-center" style={{flex:".2"}}>
-            <img className="card-img-top img-thumbnail" src="http://icons.iconarchive.com/icons/hopstarter/superhero-avatar/256/Avengers-War-Machine-icon.png" alt="User Avatar"/>
-            <div className="card-body" style={{padding:"5px"}}>
-                <h5 className="card-title text-center">{name}</h5>
-            </div>
-        </div>
-    )
-}
+const UserCard = function({ name }) {
+  return (
+    <div className="card text-center">
+      <img
+        className="card-img-top"
+        src="http://icons.iconarchive.com/icons/hopstarter/superhero-avatar/256/Avengers-War-Machine-icon.png"
+        alt="User Avatar"
+      />
+      <div className="card-body">
+        <h5 className="card-title text-center">{name}</h5>
+      </div>
+    </div>
+  );
+};
+
+const UserList = function({ userlist }) {
+  let users = userlist.map(user => {
+    // console.log(user.username)
+    return <UserCard name={user.username} />;
+  });
+
+  return <div className="card-deck">{users}</div>;
+};
+
+const EventInfo = function({name, image_url, cateogry, price, location, description}) {
+  let displayPrice = ""
+  for(let i = 0 ; i < price ; i++){
+    displayPrice += "$"
+  }
+  return (
+    // <div className="row" style={{ height: "150%" }}>
+    //   <div className="col">
+    //     <h1>I am here</h1>
+    //   </div>
+    // </div>
+    <Card>
+      <CardImg
+        top
+        width="100%"
+        src={image_url}
+        alt="Card image cap"
+      />
+      <CardBody>
+        <CardTitle>{name}</CardTitle>
+        <CardText>
+          Cateogry: {cateogry}
+        </CardText>
+        {/* Price: {displayPrice}
+          Location: {location} */}
+          <CardText>
+            Price: {displayPrice}
+          </CardText>
+          <CardText>
+            Location: {location}
+          </CardText>
+        <CardText>
+          <div className="row">
+            <Button className="btn btn-primary col mx-5" color="success">Yes! </Button>
+            <Button className="btn btn-primary col mx-5" color="danger">No</Button>
+          </div>
+        </CardText>
+        <CardText>
+          {description}
+        </CardText>
+      </CardBody>
+    </Card>
+  );
+};
 
 class RoomPage extends Component {
   constructor(props) {
-  super(props);
+    super(props);
 
-  this.state = {
-    name: "",
-    description: ""
-  };
-
-//   render(){
-//     return(
-//       <div>
-//         <div className="jumbotron">
-//           <h1 className="display-4">Room Name</h1>
-//        </div>
-//
-//        <div class="container">
-//     <div class="row">
-//       <div class="col">
-//       <div class="container">
-//    <div class="row">
-//      <div class="col">
-//        1 of 2
-//      </div>
-//      <div class="col">
-//        2 of 2
-//      </div>
-//    </div>
-//  </div>
-//       </div>
-//       <div class="col">
-//         2 of 2
-//       </div>
-//     </div>
-//   </div>
-// </div>
-//   )}
-
-
+    this.state = {
+      name: "Lorem Ipsum",
+      description: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical"
+    };
   }
-   render(){
+  render() {
+    let userlist = [{ username: "user1" }, { username: "User2" }];
 
-       let userlist = [
-         {username: "user1"},
-         {username: "User2"}
-       ]
-
-       userlist = userlist.map((user)=>{
-          // console.log(user.username)
-           return(
-               <UserCard name={user.username}/>
-           )
-       })
-
-
-
-       // console.log(userlist)
-
-       return (
-    <div>
-                 <div className="jumbotron" style={{height:"75px"}}>
-                   <h1 className="display-4">Room Name</h1>
-                </div>
-         <div className="container">
-  <div className="row">
-    <div className="col" style={{height:"500px", background:"#ADD8E6"}}>
-    <div className="card-deck" style={{padding:"5px"}}>
-       {userlist}
-    </div>
-    </div>
-    <div className="col" style={{height:"500px", background:"#E6E6FA"}}>
-      <div className="row" style={{height:"400px"}}>
-        <div className="col">
-          <h1>Yelp Suggestion Goes here...</h1>
+    return (
+      <div>
+        <div className="jumbotron" style={{ height: "25%" }}>
+          <h1 className="display-4">{this.state.name}</h1>
+          <p>{this.state.description}</p>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col mx-3">
+              <UserList userlist={userlist} />
+            </div>
+            <div className="col mx-3">
+              {/* name, image_url, cateogry, price, location */}
+              <EventInfo 
+                name={"Tasty Hand-Pulled Noodles"} 
+                image_url={"https://s3-media3.fl.yelpcdn.com/bphoto/on5kwb77QO9cS78kxllnOA/o.jpg"}
+                cateogry="American"
+                price={4}
+                location="1 Doyers St New York, NY 10013"
+                description={this.state.description}
+              />
+              <div className="row">
+                {/* <Vote /> */}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-        <div className="row" style={{padding:"20px"}}>
-          <div className="col text-center">
-            <button type="button" class="btn btn-success" style={{padding:"15px"}}>Yes</button>
-          </div>
-          <div className="col text-center">
-            <button type="button" class="btn btn-danger" style={{padding:"15px"}}>No</button>
-          </div>
-          </div>
-          </div>
-      </div>
-
-    </div>
-  </div>
-  </div>
-  </div>
-
-       )
-     }
+    );
+  }
 }
 
-export default RoomPage
+export default RoomPage;
