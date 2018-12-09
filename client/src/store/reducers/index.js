020264daa34e11f794fc91c9bd15e834ae523509
shikdernyc <shikdernyc@gmail.com>
2018-12-09 12:40:46 -0500

    Added global to combined reducer

--- client/src/store/reducers/index.js
+++ client/src/store/reducers/index.js
@@ -2,9 +2,10 @@ import { combineReducers } from "redux";
 import user from './user.js'
 import dashboard from './dashboard'
 import room from './room'
+import global from './global'
 
 const rootReducer = combineReducers({
-    user, dashboard, room
+    user, dashboard, room, global
 })
 
 export default rootReducer;
