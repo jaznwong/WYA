381428b133fba9225b692fc47760b56448d0b303
shikdernyc <shikdernyc@gmail.com>
2018-12-09 01:47:50 -0500

    Refractored server communications to services

---
+++ client/src/services/server/auth.js
@@ -0,0 +1,35 @@
+import { get, post } from "./index";
+const BASE = "/auth";
+
+export async function signup(firstname, lastname, username, password) {
+    try{
+        return await post(`${BASE}/signup`, {
+          firstname,
+          lastname,
+          username,
+          password
+        });
+    }catch(error){
+        throw `Error signing up`
+    }
+}
+
+export async function login(username, password){
+    try{
+        return await post(`${BASE}/signup`, {
+          username,
+          password,
+        });
+    }catch(error){
+        throw `Error logging up`
+    }
+}
+
+export async function logout(){
+    try{
+        let url = `${BASE}/logout`;
+        return await post(url)
+    }catch(error){
+        throw `error logging out`
+    }
+}
