27b1c98545e3d006b416183946ad37d1c3f48740
shikdernyc <shikdernyc@gmail.com>
2018-12-12 21:57:50 -0500

    Added statics for gh-pages

---
+++ precache-manifest.70313c2affb6eb6b5d1e1ccd6ad9a1b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9d4e8241109ebe98dd9d",
+    "url": "/static/js/main.9d4e8241.chunk.js"
+  },
+  {
+    "revision": "ff94f90fa21d0c6d7712",
+    "url": "/static/js/1.ff94f90f.chunk.js"
+  },
+  {
+    "revision": "9d4e8241109ebe98dd9d",
+    "url": "/static/css/main.8965b79a.chunk.css"
+  },
+  {
+    "revision": "ff94f90fa21d0c6d7712",
+    "url": "/static/css/1.0ebc33e9.chunk.css"
+  },
+  {
+    "revision": "66444be7ff417441c6ca48438c5cbd27",
+    "url": "/index.html"
+  }
+];
